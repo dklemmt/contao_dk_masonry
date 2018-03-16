447ed31999397d433791ead733eab20375cff855
fritzmg <fmg@inspiredminds.at>
2018-03-16 14:08:23 +0100

    add english translations

---
+++ system/modules/dk_masonry/languages/en/modules.php
@@ -0,0 +1,26 @@
+<?php 
+
+/**
+ * Contao Open Source CMS
+ * 
+ * Copyright (C) 2005-2015 Leo Feyer
+ * 
+ * @package   masonry
+ * @author    Dirk Klemmt
+ * @license   MIT
+ * @copyright Dirk Klemmt 2013-2015
+ */
+
+
+/**
+ * Back end modules
+ */
+$GLOBALS['TL_LANG']['MOD']['dk_masonry'][0] = 'Masonry - smart grid layout';
+
+
+/**
+ * Front end modules
+ */
+$GLOBALS['TL_LANG']['FMD']['masonry_category'] = 'Masonry';
+$GLOBALS['TL_LANG']['FMD']['masonry_gallery'][0] = 'Masonry - Gallery';
+$GLOBALS['TL_LANG']['FMD']['masonry_gallery'][1] = 'Creates a Masonry image gallery.';
